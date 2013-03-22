059a1614c5445ce38ca2484aff70a4f6475e6c98
Ewan Andreasen <ea@vejlebib.dk>
2013-03-22 12:08:52 +0100

    Providing more variables for the zerohit confirmation template

--- zerohit-confirmation.tpl.php
+++ zerohit-confirmation.tpl.php
@@ -5,17 +5,18 @@
   * Customize confirmation screen after successful submission of the zero-hit form.
   *
   * Available variables:
+  * - $webform_node: object containing data on the zerohit webform node
+  * - $submission: object containing data on this submission
   * - $confirmation_message: The confirmation message input by the zerohit webform author.
-  * - $sid: The unique submission ID of this submission.
+  * - $user_description: Description as input in user's submission.
+  * - $user_name: User's name as input in user's submission.
   */
 ?>
- 
-<div class="webform-confirmation">
+
+<div class="zerohit-confirmation">
   <?php if ($confirmation_message): ?>
     <?php print $confirmation_message ?>
   <?php else: ?>
-    <p><?php print t('ZEROHIT!!! Thank you, your submission has been received.'); ?></p>
+    <p><?php print t('Thank you, your submission has been received.'); ?></p>
   <?php endif; ?>
-  
-  sid: <?php print $sid; ?>
 </div>
